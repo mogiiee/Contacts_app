e2944be3f71faad25d658cd0c07ae1e3e518b328
Amogh Arya <amogharya2@gmail.com>
2022-06-26 23:31:45 +0530

    feat: added support for CORS

--- app/main.py
+++ app/main.py
@@ -4,11 +4,31 @@ from pydantic import BaseModel
 from typing import Union
 from fastapi.encoders import jsonable_encoder
 from bson.objectid import ObjectId
-
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
 
 class details(BaseModel):
     first_name: str
